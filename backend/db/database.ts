36f534d617610c3f4b24643e1f768052b3c160cd
ljsvdn <lajosvadnai@gmail.com>
2023-08-28 12:27:39 +0200

    sequelize database init

---
+++ backend/db/database.ts
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Initialize Sequelize with environment variables
+const sequelize = new Sequelize({
+  dialect: "mysql",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+export default sequelize;
